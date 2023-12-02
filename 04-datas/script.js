9a62285699a904dc3e2eec0418cfd4d1bd94cc28
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2023-12-02 16:52:03 -0300

    Manipulação de datas

---
+++ 04-datas/script.js
@@ -0,0 +1,24 @@
+/**
+ * - new Date() objeto pegar a data do atual do hardware que esta solicitando
+ * - getDate() - Pega da do dia atual;
+ * - getMonth() - Pega data do mês;
+ * - getHours() - Pega a hora;
+ * - getMinutes() - Pega os minutos;
+ * - getSeconds() - Pega o segundos;
+ * - getFullYear() - Pega o ano;
+ */
+
+let data = new Date();
+
+let dataDoDia = data.getDate();
+
+let dataMes = data.getMonth();
+
+let hora = data.getHours();
+
+let minutos = data.getMinutes();
+
+let segundo = data.getSeconds();
+
+let dataAno = data.getFullYear();
+
