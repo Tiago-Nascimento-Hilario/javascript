ec7a8b3e604dd79849f15eb12edaadd68fdb2ed4
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2024-10-11 22:20:34 -0300

    criado funcao para salvar pensamento

--- 06-projeto/js/api.js
+++ 06-projeto/js/api.js
@@ -4,9 +4,23 @@ const api = {
     async buscarPensamentos() {
         try {
             const response = await fetch("http://localhost:3000/pensamentos");
-            return response.json();
+            return await response.json();
         } catch (error) {
-            alert("Erro buscar pensamento");
+            alert("Erro ao buscar pensamento");
+            throw error;
+        }
+    },
+
+    async salvarPensamento(pensamento){
+        try {
+          const response = await fetch("http://localhost:3000/pensamentos",{
+            method: "POST",
+            headers:{"Content-Type":"application/json"},
+            body: JSON.stringify(pensamento)
+          });
+          return await response.json();  
+        } catch (error) {
+            alert("Erro ao buscar pensamento");
             throw error;
         }
     }
