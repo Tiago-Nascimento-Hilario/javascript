dd08243229b6dd5d3e3776f5f893f68fb6100044
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2024-10-12 20:47:09 -0300

    criado função para adicionar pensamento

--- 06-projeto/js/main.js
+++ 06-projeto/js/main.js
@@ -1,5 +1,30 @@
 import ui from "./ui.js";
+import api from "./api.js";
+
+
+async function controlarEnvioFormulario(event){
+    event.preventDefault();
+    
+    const id  = document.querySelector("#pensamento-id").value;
+    const conteudo = document.querySelector("#pensamento-conteudo").value;
+    const autoria = document.querySelector("#pensamento-autoria").value;
+
+    try {
+        await api.salvarPensamento({conteudo, autoria})
+        ui.rederizarPensamentos();
+    } catch {
+        alert("Ocorreu um erro ao salvar pensamento");
+    }
+}
+
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
+ 
     ui.rederizarPensamentos();
+
+    const formularioPensamento = document.querySelector("#pensamento-form");
+    formularioPensamento.addEventListener("submit", controlarEnvioFormulario)
+
 })
+
