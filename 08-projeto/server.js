f6c9dd824f9341f67bc0367bd91794925d96bcd5
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2024-10-18 00:24:01 -0300

    Instalado o Express

--- 08-projeto/server.js
+++ 08-projeto/server.js
@@ -1,17 +1,17 @@
 import http from "http";
+import app from "./src/api.js"
 
+// const rotas = {
+//     "/" : "Primeira rota",
+//     "/segunda" : "Segunda rota",
+//     "/terceira" : "Terceira rota"
+// }
 
-const rotas = {
-    "/" : "Primeira rota",
-    "/segunda" : "Segunda rota",
-    "/terceira" : "Terceira rota"
-}
+// const servidor = http.createServer((req, res)=>{
+//     res.writeHead(200, {"Content-type":"text/plain"});
+//     res.end(rotas[req.url])
+// })
 
-const servidor = http.createServer((req, res)=>{
-    res.writeHead(200, {"Content-type":"text/plain"});
-    res.end(rotas[req.url])
-})
-
-servidor.listen(3000, () => {
+app.listen(3000, () => {
     console.log("Execuntando servidor");
 })
